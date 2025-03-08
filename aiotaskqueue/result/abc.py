819ebc149eb809aa1e1e9b1856d05de28284f99c
Doctor <thirvondukr@gmail.com>
2025-03-09 01:21:47 +0300

    feat: add ResultBackend.get method

--- aiotaskqueue/result/abc.py
+++ aiotaskqueue/result/abc.py
@@ -1,10 +1,16 @@
-from typing import Protocol
+from typing import Any, Protocol
 
 from aiotaskqueue._types import TResult
-from aiotaskqueue.tasks import RunningTask
+from aiotaskqueue.tasks import RunningTask, TaskDefinition
 
 
 class ResultBackend(Protocol):
     async def set(self, task_id: str, value: TResult) -> None: ...
+
+    async def get(
+        self,
+        task_id: str,
+        definition: TaskDefinition[Any, TResult],
+    ) -> TResult | None: ...
 
     async def wait(self, task: RunningTask[TResult]) -> TResult: ...
--- aiotaskqueue/result/redis.py
+++ aiotaskqueue/result/redis.py
@@ -1,12 +1,13 @@
 import asyncio
-from typing import cast
+from datetime import timedelta
+from typing import Any, cast
 
 from aiotaskqueue._types import TResult
 from aiotaskqueue.broker.redis import RedisClient
 from aiotaskqueue.config import Configuration
 from aiotaskqueue.result.abc import ResultBackend
 from aiotaskqueue.serialization import SerializationBackendId, serialize
-from aiotaskqueue.tasks import RunningTask
+from aiotaskqueue.tasks import RunningTask, TaskDefinition
 
 
 class RedisResultBackend(ResultBackend):
@@ -14,9 +15,11 @@ class RedisResultBackend(ResultBackend):
         self,
         redis: RedisClient,
         configuration: Configuration,
+        poll_interval: timedelta = timedelta(milliseconds=100),
     ) -> None:
         self._redis = redis
         self._config = configuration
+        self._poll_interval = poll_interval.total_seconds()
 
     async def set(self, task_id: str, value: TResult) -> None:
         backend_id, serialized_value = serialize(
@@ -30,20 +33,47 @@ class RedisResultBackend(ResultBackend):
             ex=self._config.result.result_ttl,
         )
 
+    async def get(
+        self,
+        task_id: str,
+        definition: TaskDefinition[Any, TResult],
+    ) -> TResult | None:
+        raw_value = await self._redis.get(self._cache_key(task_id))
+        if raw_value is None:
+            return None
+
+        result = await self._deserialize(
+            raw_value,
+            return_type=definition.return_type,
+        )
+        return cast(TResult, result)
+
     async def wait(
         self,
         task: RunningTask[TResult],
         *,
-        poll_interval: float = 0.1,
+        poll_interval: float | None = None,
     ) -> TResult:
+        poll_interval = poll_interval or self._poll_interval
+
         while not (raw_value := await self._redis.get(self._cache_key(task.id))):  # noqa: ASYNC110
             await asyncio.sleep(poll_interval)
 
+        result = await self._deserialize(
+            raw_value,
+            return_type=task.instance.task.return_type,
+        )
+        return cast(TResult, result)
+
+    async def _deserialize(
+        self,
+        raw_value: bytes,
+        return_type: type[Any],
+    ) -> Any:  # noqa: ANN401
         backend_id, value = raw_value.split(b",", maxsplit=1)
-        deserialized = self._config.serialization_backends[
+        return self._config.serialization_backends[
             SerializationBackendId(backend_id.decode())
-        ].deserialize(value=value, type=task.instance.task.return_type)
-        return cast(TResult, deserialized)
+        ].deserialize(value=value, type=return_type)
 
     def _cache_key(self, task_id: str) -> str:
         return self._config.result.result_key(task_id)
