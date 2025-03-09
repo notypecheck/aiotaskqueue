8a3bb892eb3d3d922367461b0193c6bfc41e176b
Doctor <thirvondukr@gmail.com>
2025-03-09 03:32:49 +0300

    fix: skip tasks in "sequential" if there's a result present already

--- aiotaskqueue/experimental/sequential.py
+++ aiotaskqueue/experimental/sequential.py
@@ -35,20 +35,26 @@ async def sequential_task(
     seq: Sequential,
     context: ExecutionContext = INJECTED,
 ) -> None:
-    if not seq.tasks:
-        return
-
-    if not context.result_backend:
+    if not context.result_backend:  # pragma: no cover
         err_msg = "Result backend must be enabled in order to use sequential"
         raise ValueError(err_msg)
 
     for task_record in seq.tasks:
         task_definition = context.tasks.tasks[task_record.task_name]
 
+        result = await context.result_backend.get(
+            task_id=task_record.id,
+            definition=task_definition,
+        )
+        if result is not None:
+            continue
+
         args, kwargs = deserialize_task(
             task_definition=task_definition,
             task=task_record,
             serialization_backends=context.configuration.serialization_backends,
         )
-        job = await context.publisher.enqueue(task_definition(*args, **kwargs))
+        job = await context.publisher.enqueue(
+            task_definition(*args, **kwargs), id=task_record.id
+        )
         await context.result_backend.wait(job)
--- tests/experiments/test_sequential.py
+++ tests/experiments/test_sequential.py
@@ -1,13 +1,28 @@
 import asyncio
 import random
 
+import pytest
 from aiotaskqueue import Configuration, Publisher, TaskParams, TaskRouter, task
 from aiotaskqueue.broker.abc import Broker
-from aiotaskqueue.experimental.sequential import sequential, sequential_task
+from aiotaskqueue.experimental.sequential import Sequential, sequential, sequential_task
 from aiotaskqueue.result.abc import ResultBackend
-from aiotaskqueue.worker import AsyncWorker
+from aiotaskqueue.worker import AsyncWorker, ExecutionContext
 
 from tests.utils import run_worker_until
+
+
+@pytest.fixture
+def execution_context(
+    configuration: Configuration,
+    publisher: Publisher,
+    redis_result_backend: ResultBackend,
+) -> ExecutionContext:
+    return ExecutionContext(
+        configuration=configuration,
+        publisher=publisher,
+        result_backend=redis_result_backend,
+        tasks=TaskRouter([sequential_task]),
+    )
 
 
 async def test_sequential(
@@ -44,3 +59,61 @@ async def test_sequential(
         )
 
     assert results == list(range(count))
+
+
+@pytest.mark.parametrize(
+    ("total_tasks", "random_tasks_to_mark_as_completed"),
+    [(3, 1), (3, 3), (3, 0), (10, 5)],
+)
+async def test_should_skip_executed_tasks(
+    broker: Broker,
+    execution_context: ExecutionContext,
+    total_tasks: int,
+    random_tasks_to_mark_as_completed: int,
+) -> None:
+    assert execution_context.result_backend
+    finished = asyncio.Event()
+
+    if total_tasks == random_tasks_to_mark_as_completed:
+        finished.set()
+
+    results = []
+
+    @execution_context.tasks.task(TaskParams(name="test-task"))
+    async def test_task(n: int) -> None:
+        results.append(n)
+        if len(results) == total_tasks - random_tasks_to_mark_as_completed:
+            finished.set()
+
+    tasks = [test_task(i) for i in range(total_tasks)]
+
+    sequential_task = sequential(*tasks, configuration=execution_context.configuration)
+    record: Sequential = sequential_task.kwargs["seq"]  # type: ignore[assignment]
+    for random_task in random.sample(
+        record.tasks,
+        k=random_tasks_to_mark_as_completed,
+    ):
+        await execution_context.result_backend.set(random_task.id, value=None)
+
+    expected_result = [
+        value
+        for record, instance, value in zip(
+            record.tasks, tasks, range(total_tasks), strict=True
+        )
+        if await execution_context.result_backend.get(
+            record.id, definition=instance.task
+        )
+        is None
+    ]
+
+    worker = AsyncWorker(
+        broker=broker,
+        result_backend=execution_context.result_backend,
+        configuration=execution_context.configuration,
+        concurrency=10,
+        tasks=execution_context.tasks,
+    )
+    async with run_worker_until(worker, finished):
+        await execution_context.publisher.enqueue(sequential_task)
+
+    assert results == expected_result
