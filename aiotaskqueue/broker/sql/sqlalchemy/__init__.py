628270fcf3977d712b215090db5ebe839ba69244
maksyutov.vlad <maksyutov.vlad@gmail.com>
2025-10-06 10:20:05 +0300

    fix: ruff

--- aiotaskqueue/broker/sql/sqlalchemy/__init__.py
+++ aiotaskqueue/broker/sql/sqlalchemy/__init__.py
@@ -1,9 +1,11 @@
-from aiotaskqueue.broker.sql.sqlalchemy.broker import SqlalchemyPostgresBroker, SqlalchemyBrokerConfig
+from aiotaskqueue.broker.sql.sqlalchemy.broker import (
+    SqlalchemyBrokerConfig,
+    SqlalchemyPostgresBroker,
+)
 from aiotaskqueue.broker.sql.sqlalchemy.models import PostgresBrokerTaskMixin
 
-
 __all__ = [
-    "SqlalchemyPostgresBroker",
-    "SqlalchemyBrokerConfig",
     "PostgresBrokerTaskMixin",
+    "SqlalchemyBrokerConfig",
+    "SqlalchemyPostgresBroker",
 ]
--- aiotaskqueue/result/sql/sqlalchemy/__init__.py
+++ aiotaskqueue/result/sql/sqlalchemy/__init__.py
@@ -4,9 +4,8 @@ from aiotaskqueue.result.sql.sqlalchemy.backend import (
 )
 from aiotaskqueue.result.sql.sqlalchemy.models import PostgresResultTaskMixin
 
-
 __all__ = [
+    "PostgresResultTaskMixin",
     "SqlalchemyPostgresResultBackend",
     "SqlalchemyResultBackendConfig",
-    "PostgresResultTaskMixin",
 ]
